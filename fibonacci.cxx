61574f7c5eec5737ffe9ac2c198ef7fd83d74c34
Konstantin Beyer <kobey100@pc06.pool.tp1.uni-duesseldorf.de>
2015-11-02 11:13:31 +0100

    Programm zur Fobonacci Folge wurde geschrieben.

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b,c,N;
+cout <<"N"<<endl;
+cin>>N;
+a=0;b=1;
+if(N==0){cout<<a<< endl;}
+else {if(N==1){cout<<b<< endl;}
+     else{
+        for(int i=2;i<=N;i++){
+         c=a+b;
+         a=b;
+         b=c;
+        }
+  cout<<c<<endl;
+}}
+return 0;
+}
 
